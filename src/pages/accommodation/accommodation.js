2becbf97bbd9283823b7e46b3dbd19d3c9ddf20c
Josh Urdnb <joshua.urdanabia@gmail.com>
2024-11-07 16:23:57 +0100

    Added Collapse component, displaying the equipment list without default styling.

--- src/pages/accommodation/accommodation.js
+++ src/pages/accommodation/accommodation.js
@@ -3,20 +3,33 @@ import { useParams } from 'react-router-dom' // Pour accéder aux paramètres d'
 import { kasaData } from '../../datas/data' // Importe les données de logements
 import Header from '../../layout/header/Header'
 import Footer from '../../layout/footer/Footer'
+import Collapse from '../../components/collapse/Collapse'
+
 
 export default function Accommodation() {
-    const { id } = useParams() // Récupère l'ID du logement depuis l'URL
-    const housingData = kasaData.find((housing) => housing.id === id) // Cherche le logement correspondant dans kasaData
+	const { id } = useParams() // Récupère l'ID du logement depuis l'URL
+	const housingData = kasaData.find((housing) => housing.id === id) // Cherche le logement correspondant dans kasaData
 
-    return (
-        <div className='accommodation'>
-            <Header />
-            <div className="accommodation-details">
-                <h1 className="accommodation-title">{housingData.title}</h1>
-                <p className="accommodation-location">{housingData.location}</p>
-                <p className="accommodation-description">{housingData.description}</p>
-            </div>
-            <Footer />
-        </div>
-    )
+	return (
+		<div className='accommodation'>
+			<Header />
+			<div className="accommodation-container">
+				<div className="accommodation-details">
+					<h1 className="accommodation-title">{housingData.title}</h1>
+					<p className="accommodation-location">{housingData.location}</p>
+				</div>
+				<div className="collapses">
+					<Collapse title="Description" content={housingData.description} />
+					<Collapse title="Équipments" content={
+						<ul className="collapse-style-list" >
+							{housingData.equipments.map((equipment, index) => (
+								<li key={index}>{equipment}</li>
+							))}
+						</ul>
+					} />
+				</div>
+			</div>
+			<Footer />
+		</div>
+	)
 }
