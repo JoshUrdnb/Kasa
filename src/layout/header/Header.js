b6007a2f65eb2903705fe6e0037191a1955e57cb
Josh Urdnb <joshua.urdanabia@gmail.com>
2024-11-15 16:49:49 +0100

    Add dynamic active link styling in Header based on current route using useLocation.

--- src/layout/header/Header.js
+++ src/layout/header/Header.js
@@ -1,14 +1,17 @@
-import { Link } from 'react-router-dom'
+import { Link, useLocation } from 'react-router-dom'
 import logo from '../../assets/logo.png'
 import './header.css'
 
 function Header() {
+
+	const location = useLocation()
+
 	return (
 		<div className='kasa-banner'>
 			<Link to="/"><img src={logo} alt='Kasa, le site immobilier' className='kasa-logo' /></Link>
 			<div className='nav-links'>
-				<Link to="/">Accueil</Link>
-				<Link to="/about">À Propos</Link>
+				<Link to="/" className={location.pathname === "/" ? "active-link" : ""}>Accueil</Link>
+				<Link to="/about" className={location.pathname === "/about" ? "active-link" : ""}>À Propos</Link>
 			</div>
 		</div>
 	)
