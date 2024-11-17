87a10e84562c43c271c0f31bcf30b428a686f604
Josh Urdnb <joshua.urdanabia@gmail.com>
2024-11-17 14:25:55 +0100

    Refactored router to use <Routes> and <Route> for defining routes.

--- src/App.js
+++ src/App.js
@@ -1,40 +1,23 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Layout from './layout/Layout'
 import Home from './pages/home/Home'
 import Accommodation from './pages/accommodation/Accommodation'
 import About from './pages/about/About'
 import Error from './pages/error/Error'
-import {createBrowserRouter, RouterProvider} from "react-router-dom"
-
-const router = createBrowserRouter([
-    {
-        element: <Layout />, // J'utilise Layout comme wrapper pour les routes principales
-        children: [
-            {
-                path: "/",
-                element: <Home />,
-            },
-            {
-                path: "/accommodation/:id",
-                element: <Accommodation />,
-            },
-            {
-                path: "/about",
-                element: <About />,
-            },
-            {
-                path: "*",
-                element: <Error />,
-            },
-        ],
-    },
-])
 
 function App() {
-	return (
-		<div>
-			<RouterProvider router={router} />
-		</div>
-	)
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route element={<Layout />}>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/accommodation/:id" element={<Accommodation />} />
+                    <Route path="/about" element={<About />} />
+                    <Route path="*" element={<Error />} />
+                </Route>
+            </Routes>
+        </BrowserRouter>
+    )
 }
 
 export default App
