375440bd0adec21c6656ba2922b68d8877af7fa6
Josh Urdnb <joshua.urdanabia@gmail.com>
2024-11-18 16:44:10 +0100

    Refactored accommodation component structure.

--- src/pages/accommodation/accommodation.js
+++ src/pages/accommodation/accommodation.js
@@ -1,8 +1,6 @@
 import './accommodation.css'
 import { useParams } from 'react-router-dom' // Pour accéder aux paramètres d'URL
 import { kasaData } from '../../datas/data' // Importe les données de logements
-// import Header from '../../layout/header/Header'
-// import Footer from '../../layout/footer/Footer'
 import Carousel from '../../components/carousel/Carousel'
 import Tags from '../../components/tags/Tags'
 import Profile from '../../components/profile/Profile'
@@ -14,59 +12,56 @@ export default function Accommodation() {
 	const housingData = kasaData.find((housing) => housing.id === id) // Cherche le logement correspondant dans kasaData
 
 	return (
-		<div className='accommodation-main'>
+		<section className='accommodation'>
 
-			{/* <Header /> */}
+			<Carousel className="carousel" gallery={housingData} />
 
-			<div className='accommodation'>
-
-				<div className="gallery-accommodation-container">
-					<Carousel gallery={housingData} />
-				</div>
-
-				<div className="accommodation-container">
+			<div className="accommodation-container">
+				<div className="accommodation-infos">
 					<div className="accommodation-details">
 						<h1 className="accommodation-title">{housingData.title}</h1>
 						<p className="accommodation-location">{housingData.location}</p>
 					</div>
 
-					<div className="tags-container">
-						{
-							housingData.tags.map((tag, index) =>
+					<div className="tag-rating">
+						<div className="tags-container">
+							{housingData.tags.map((tag, index) => (
 								<Tags key={index} tag={tag} />
-							)
-						}
+							))}
+						</div>
+						{/* <Rating className="rating" rate={housingData.rating} /> */}
 					</div>
+				</div>
 
-					<div className="profile-container">
-						<Profile host={housingData.host} />
-					</div>
-
-					<div className="rating-container">
-						<Rating rate={housingData.rating} />
-					</div>
-
-					<div className="collapses-container">
-
-						<Collapse title="Description">
-							{housingData.description}
-						</Collapse>
-
-						<Collapse title="Équipments">
-							{
-								<ul className="collapse-style-list" >
-									{housingData.equipments.map((equipment, index) => (
-										<li key={index}>{equipment}</li>
-									))}
-								</ul>
-							}
-						</Collapse>
-					</div>
+				<div className="accommodation-profile">
+					<Profile className="profile" host={housingData.host} />
+					<Rating className="rating" rate={housingData.rating} />
 				</div>
 			</div>
 
-			{/* <Footer /> */}
+			{/* <div className="tag-rating">
+				<div className="tags-container">
+					{housingData.tags.map((tag, index) => (
+						<Tags key={index} tag={tag} />
+					))}
+				</div>
+				<Rating className="rating" rate={housingData.rating} />
+			</div> */}
 
-		</div>
+			<div className="collapses-container">
+				<Collapse title="Description">
+					{housingData.description}
+				</Collapse>
+
+				<Collapse title="Équipments">
+					<ul className="collapse-style-list">
+						{housingData.equipments.map((equipment, index) => (
+							<li key={index}>{equipment}</li>
+						))}
+					</ul>
+				</Collapse>
+			</div>
+
+		</section>
 	)
 }
